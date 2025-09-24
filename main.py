89755e57e275f3a2af8c37a5fd4e193fce32cf85
Yusuf <86446300+Aermoss@users.noreply.github.com>
2025-09-24 23:37:49 +0300

    Update build script.

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main(argv):
     module.triple = llvm.get_default_triple()
 
     sdk_path = "C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\"
+    print(os.listdir(sdk_path))
     sdk_path += max(os.listdir(sdk_path), key = version.parse)
 
     vs_path = "C:\\Program Files\\Microsoft Visual Studio\\"
